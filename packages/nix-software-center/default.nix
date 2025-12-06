a6d0b525d73e21644d8f55caa67c0be65fdd0c16
bahrom04 <magdiyevbahrom@gmail.com>
2025-12-06 12:23:04 +0500

    chore: update lock

--- packages/nix-software-center/default.nix
+++ packages/nix-software-center/default.nix
@@ -31,7 +31,7 @@ in
     cargoDeps = rustPlatform.importCargoLock {
       lockFile = ../../Cargo.lock;
       outputHashes = {
-        "nix-data-0.0.3" = "sha256-dhEGLa8BXWfAVJXBlpRvP15ke0+PpqYa8I5AuFoV/2Y=";
+        "nix-data-0.0.3" = "sha256-0xp0Z4SemagJl2FSuCaEb+zIWpiT+lvcfv8IWtJqOk0=";
       };
     };
 
