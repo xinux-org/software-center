a69b6d7da515103d701298516d31ac0108944b26
bahrom04 <magdiyevbahrom@gmail.com>
2025-12-11 18:18:28 +0500

    chore: update release number

--- flake.nix
+++ flake.nix
@@ -4,12 +4,12 @@
   inputs = {
     # Stable for keeping thins clean
     # # Fresh and new for testing
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    crane.url = "github:ipetkov/crane";    
+    crane.url = "github:ipetkov/crane";
     # The flake-utils library
     flake-utils.url = "github:numtide/flake-utils";
-    
+
     nixos-appstream-data = {
       url = "github:korfuri/nixos-appstream-data/flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -35,11 +35,11 @@
 
       # Output package
       packages.default = pkgs.callPackage ./. {inherit crane pkgs inputs;};
-    });
-  # // {
-  #   # Hydra CI jobs
-  #   hydraJobs = {
-  #     packages = self.packages.x86_64-linux.default;
-  #   };
-  # };
+    })
+    // {
+      # Hydra CI jobs
+      hydraJobs = {
+        packages = self.packages.x86_64-linux.default;
+      };
+    };
 }
