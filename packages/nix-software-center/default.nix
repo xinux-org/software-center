98ba2a6c33c5fa19249ad18b4a8bce63065872db
bahrom04 <magdiyevbahrom@gmail.com>
2026-07-21 18:58:57 +0500

    release 0.2.0 after code refactor

--- packages/nix-software-center/default.nix
+++ packages/nix-software-center/default.nix
@@ -24,13 +24,13 @@ let
 in
 stdenv.mkDerivation {
   pname = "nix-software-center";
-  version = "0.1.4";
+  version = "0.2.0";
 
   src = [ ../.. ];
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     src = ../..;
-    hash = "sha256-UVuavPyQk6bH6rNuwcJvFxwn/cKp/NnCDJDR2mTOvV0=";
+    hash = "sha256-gjcBsl4N4CCsiSxtZBu8nrcTHhyJPepBF4lJytS4AtY=";
   };
 
   nativeBuildInputs =
